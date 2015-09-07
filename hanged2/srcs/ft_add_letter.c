9771de0ec9e13a1b17232497fbf9aed3a4a9dc9c
Tu-tur <arthur15400@gmail.com>
2015-09-07 13:06:12 +0200

    fixing add letter.c

--- hanged2/srcs/ft_add_letter.c
+++ hanged2/srcs/ft_add_letter.c
@@ -5,7 +5,7 @@ char	*ft_add_letter(char *str, char c)
 {
 	char	*cat_str;
 
-	cat_str = malloc(sizeof(char) * (ft_strlen(str) + 1));
+	cat_str = malloc(sizeof(char) * (ft_strlen(str) + 2));
 	ft_strcpy(cat_str, str);
 	cat_str[ft_strlen(str)] = c;
 	cat_str[ft_strlen(str) + 1] = '\0';
